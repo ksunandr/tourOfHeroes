ca500856837ad30557d18d2a2dab5cad882b0a7e
ksunandr <ksun.andr@gmail.com>
2020-05-04 17:35:41 +0300

    hero detail and hero service

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +8,17 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  public heroes: Hero[] = HEROES;
+  public heroes: Hero[];
 
   public selectedHero: Hero;
-  constructor() {
+  constructor(private heroService: HeroService) {
+  }
+  public getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
   ngOnInit(): void {
-    const forDebug = 1;
+    this.getHeroes();
   }
   onSelect(hero: Hero) {
     this.selectedHero = hero;
