87f871676bd1348332741a9ed01a42fe9e9b50cc
ksunandr <ksun.andr@gmail.com>
2020-05-04 18:19:59 +0300

    using messages

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
 import { HeroService } from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,7 @@ export class HeroesComponent implements OnInit {
   public heroes: Hero[];
 
   public selectedHero: Hero;
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
 
   public getHeroes(): void {
@@ -23,5 +24,6 @@ export class HeroesComponent implements OnInit {
   }
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add(`HeroComponent: Selected hero id = ${hero.id}`)
   }
 }
