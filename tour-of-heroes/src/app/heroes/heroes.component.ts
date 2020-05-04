520ef94ade5cf8f2c89e02f67b09cd39a4a09e7e
ksunandr <ksun.andr@gmail.com>
2020-05-04 15:48:07 +0300

    hero list

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from './hero';
-
+import { SuperHero } from '../superHero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
+  public heroes: SuperHero[] = HEROES;
+ public superHero: SuperHero = {
     id : 1,
     name: 'Windstorm'
   };
